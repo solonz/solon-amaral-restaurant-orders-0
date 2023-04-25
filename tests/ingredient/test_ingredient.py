7a3f91731ef2c662f0ea8c497332e5fedb18387c
Solon <amaral.solon@hotmail.com>
2023-04-25 14:43:16 -0300

    Req 1 - Test Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,21 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient("Tomato")
+    assert isinstance(ingredient, Ingredient)
+
+    assert ingredient.restrictions == set()
+
+    assert repr(ingredient) == "Ingredient('Tomato')"
+
+    ingredient1 = Ingredient("Tomato")
+    ingredient2 = Ingredient("Tomato")
+    ingredient3 = Ingredient("Potato")
+
+    assert ingredient1 == ingredient2
+    assert ingredient1 != ingredient3
+
+    assert hash(ingredient1) == hash(ingredient2)
+    assert hash(ingredient1) != hash(ingredient3)
+
+    assert ingredient.name == "Tomato"

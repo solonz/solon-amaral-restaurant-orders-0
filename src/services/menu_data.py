2c732bb1eebb1c1f47e8bb0e89496bccd40bb1b4
Solon <amaral.solon@hotmail.com>
2023-04-26 16:02:41 -0300

    Req 3 - MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,18 @@
+import pandas as pd
+from src.models.ingredient import Ingredient
+from src.models.dish import Dish
+
+
 # Req 3
 class MenuData:
-    def __init__(self, source_path: str) -> None:
-        pass
+    def __init__(self, source_path: str):
+        data = pd.read_csv(source_path)
+        menu = {}
+        # https://www.w3schools.com/python/pandas/ref_df_itertuples.asp
+        for dish, price, ing, amount in data.itertuples(
+            index=False
+        ):
+            if dish not in menu:
+                menu[dish] = Dish(dish, price)
+            menu[dish].add_ingredient_dependency(Ingredient(ing), amount)
+        self.dishes = set(menu.values())
